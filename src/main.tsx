ab037bc1be93b57ad977781296147f1000b819c4
Greg-Soap <emmanuelkingsley36@gmail.com>
2024-10-19 01:55:23 +0100

    adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
-import NovelViewer from "./components/novel-veiwer";
 import Root from "./pages/root";
 import Novels from "./pages/novels";
 import { ThemeProvider } from "./components/theme-provider";
